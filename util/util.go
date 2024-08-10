d07d9ed60a33ce52e1583c0460e55f9a2e1ff0f5
irfandoart1626 <irfandoirfando44@gmail.com>
2024-08-10 15:13:12 +0700

    feat|refactor telkomsel to phincon

--- util/util.go
+++ util/util.go
@@ -1,11 +1,11 @@
 package util
 
 import (
-	"cicd-gitlab-ee.telkomsel.co.id/phincon-go/common-service/log"
 	"crypto/rand"
 	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
+	"gitlab.com/phincon/middleware/rnd/notification-system/nois-go/common-service/log"
 	"math"
 	"math/big"
 	"os"
